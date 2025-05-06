391857fbe42db8e9635cad30f3385a75c3f2169c
Hermes Barbosa <hermes.barbosa@imobstylus.com>
2025-05-06 07:47:24 -0300

    Problem1 of leetcode resolved

---
+++ leetcode/problem9.py
@@ -0,0 +1,13 @@
+# 9. Palindrome Number
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
+        
+instance = Solution()
+print(instance.isPalindrome(121))
